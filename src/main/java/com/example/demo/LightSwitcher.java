c8a6602383920a7a538118558db0f77103ad8cba
oleksandrZhavoronkov <ansientsessentials@gmail.com>
2022-04-26 17:22:06 +0300

    Added: static Fields (voltage and resistance) static Methods (lightSwitcherVoltageShow and lightSwitcherCurrentPower)

--- src/main/java/com/example/demo/LightSwitcher.java
+++ src/main/java/com/example/demo/LightSwitcher.java
@@ -6,7 +6,26 @@ class lightSwitcher {
     public int buttonsQuantity;
     public float amperes;
     public boolean remoteControl;
-    public int x;
+    //Declared static fields for static methods
+    public static float voltage;
+    public static float resistance;
+
+    //initializing variables for static methods (wanted to have there some digits)
+    static {
+         voltage = 220;
+         resistance = 1200;
+    }
+
+    //Static method creation for showing how much Voltage is
+    public static void lightSwitcherVoltageShow () {
+        System.out.println("All Light Switchers voltage is - " + voltage + " " + "Volt");
+    }
+
+    //Static method creation for calculating how much current Power is
+    public static float lightSwitcherCurrentPower () {
+        //used formula here Voltage^2/Resistance
+        return voltage*voltage/resistance;
+    }
 
     //Constructor of class lightSwitcher creation (4 parameters)
     public lightSwitcher(String color, int buttonsQuantity, float amperes, boolean remoteControl){
@@ -38,6 +57,7 @@ class lightSwitcher {
                 ", remoteControl=" + remoteControl +
                 '}';
     }
+
     //This one for displaying 3 parameters only, without 0 or nulls
     public String toString(int buttonsQuantity, float amperes, boolean remoteControl) {
         return "lightSwitcher{" +
@@ -74,6 +94,10 @@ class lightSwitcher {
         System.out.println(bathroom);
         //Displaying an object with 1 parameter
         System.out.println(bedroom.toString("Red"));
+        //Using method lightSwitcherVoltageShow called from class lightSwitcher
+        lightSwitcher.lightSwitcherVoltageShow();
+        //Using method lightSwitcherCurrentPower called from class lightSwitcher, adding data to println function
+        System.out.println("Current Power is - " + lightSwitcher.lightSwitcherCurrentPower() + " Watt");
     }
 
 }
